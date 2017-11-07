e27cf526e23e1e9cbf895dd6a768f2b7c1a82c3d
garima1125 <garima.arya1101@gmail.com>
2017-11-07 16:51:59 +0000

    download avatar file and ignores the git token in other file

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,19 @@
+var secret = require('./secret');
+
 function getRepoContributors(repoOwner, repoName, cb) {
- var request = require('request');
- console.log('Welcome to the GitHub Avatar Downloader!');
- request.get('https://api.github.com/repos/jquery/jquery/contributors')
-    .on('error', function (err) {                                   // Note 2
-         throw err; 
-       })
-       .on('response', function (response) {                           // Note 3
-         });
+	console.log('Welcome to the GitHub Avatar Downloader!');
+  var request = require('request');
+ var options= {
+ 	
+ 	url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+  headers:{
+  	'User-Agent':secret.GITHUB_TOKEN
+  }
+};
+  request(options, function(err, res, body) {
+    
+    cb(err, body);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
