1dcbb1b3cc5818a531a2c722deaba335663e5ad9
garima1125 <garima.arya1101@gmail.com>
2017-11-07 18:55:00 +0000

    command line inputs

--- download_avatars.js
+++ download_avatars.js
@@ -1,15 +1,18 @@
+var args = process.argv;
+var repoOwner = args[2];
+var repoName = args[3]; 
 var secret = require('./secret');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-	console.log('Welcome to the GitHub Avatar Downloader!');
+  console.log('Welcome to the GitHub Avatar Downloader!');
   var request = require('request');
- var options= {
+  var options = {
  	
- 	url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-  headers:{
-  	'User-Agent':secret.GITHUB_TOKEN
-  }
-};
+ 	  url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers:{
+  	  'User-Agent':secret.GITHUB_TOKEN
+    }
+  };
   request(options, function(err, res, body) {
     
     cb(err, body);
@@ -19,30 +22,36 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
 function downloadImageByURL(url, filePath) {
  
-var request = require('request');
-var fs = require('fs');
-console.log('Downloading Image');
-request.get(url)               // Note 1
-       .on('error', function (err) {                                   // Note 2
-         throw err; 
-       })
-       .on('response', function (response) {                           // Note 3
-         console.log('Response content type:',response.headers['content-type']);
+  var request = require('request');
+  var fs = require('fs');
+  console.log('Downloading Image');
+  request.get(url)               // Note 1
+         .on('error', function (err) {                                   // Note 2
+           throw err; 
+         })
+         .on('response', function (response) {                           // Note 3
+           console.log('Response content type:',response.headers['content-type']);
 
-       })
-       .pipe(fs.createWriteStream(filePath)); 
+         })
+         .pipe(fs.createWriteStream(filePath)); 
 
-console.log('Download complete.');
+  console.log('Download complete.');
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+if(repoOwner === undefined || repoName === undefined){
+  console.log("You're missing an argument");
+} else {
+  getRepoContributors(repoOwner, repoName, function(err, result) {
   // console.log("Errors:", err);
   // console.log("Result:", result);
-  var temp = JSON.parse(result);
-  for (var r in temp){
+
+    console.log(repoOwner);
+    console.log(repoName);
+    var temp = JSON.parse(result);
+    for (var r in temp){
   	var url = temp[r].avatar_url;
   	var filePath = "avatars/" + temp[r].login +".jpg";
   	downloadImageByURL(url, filePath);
-  }
-
-});
+    }
+  });
+}
